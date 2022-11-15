b76cee7ba67981fae23903905e31c72ad36af42d
Helmi-Megdiche <helmi.megdiche@esprit.tn>
2022-11-15 00:36:32 +0000

    it s me again

---
+++ personnel.h
@@ -0,0 +1,31 @@
+#ifndef PERSONNEL_H
+#define PERSONNEL_H
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+class personnel
+{
+    QString ID_personnel ,Prenom, Nom ,Num_Tel ;
+public:
+    personnel();
+    personnel(QString,QString,QString,QString);
+    QString getID_personnel();
+    QString getPrenom();
+    QString getNom();
+    QString getNum_Tel();
+
+    void setID_personnel(QString);
+    void setPrenom(QString);
+    void setNom(QString);
+    void setNum_Tel(QString);
+
+
+    bool ajouter();
+    QSqlQueryModel * afficher ();
+    bool supprimer(QString);
+    bool modifierav();
+    QSqlQueryModel * chercher(QString);
+
+};
+
+#endif // PERSONNEL_H
